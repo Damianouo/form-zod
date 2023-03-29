0bd6419441141794b5f9bd7debf44c88f5ec638c
Damian <damianouo0@gmail.com>
2023-03-29 17:57:02 +0800

    react form with zod

---
+++ OkayAlright.js
@@ -0,0 +1,12 @@
+const register = {
+  firstName: 'fir',
+  lastName: 'las',
+  email: 'ema',
+  age: 'ag',
+  password: 'pas',
+  confirmPassword: 'confir',
+};
+
+let des = [{ ...register }, 'sss'];
+
+console.log(des);
